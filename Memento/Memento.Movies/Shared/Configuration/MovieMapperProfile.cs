950143bd85485ac5ab3bda49042c61dac464169f
Tiago Reis <nuno-tiago-reis@outlook.pt>
2020-05-27 16:35:15 +0100

    AutoMapper:
    Added missing mappings.

--- Memento/Memento.Movies/Shared/Configuration/MovieMapperProfile.cs
+++ Memento/Memento.Movies/Shared/Configuration/MovieMapperProfile.cs
@@ -1,7 +1,14 @@
 ﻿using AutoMapper;
+using Memento.Movies.Shared.Models;
 using Memento.Movies.Shared.Models.Contracts.Genres;
+using Memento.Movies.Shared.Models.Contracts.Movies;
+using Memento.Movies.Shared.Models.Contracts.Persons;
 using Memento.Movies.Shared.Models.Repositories.Genres;
+using Memento.Movies.Shared.Models.Repositories.Movies;
+using Memento.Movies.Shared.Models.Repositories.Persons;
 using Memento.Shared.Configuration;
+using System;
+using System.Linq;
 
 namespace Memento.Movies.Shared.Configuration
 {
@@ -31,21 +38,52 @@ namespace Memento.Movies.Shared.Configuration
 			#region [Contracts: Genres]
 			// Genres: Model => Contract
 			this.CreateMap<Genre, GenreDetailContract>();
-
 			// Genres: Model => Contract
 			this.CreateMap<Genre, GenreListContract>();
 
 			// Genres: Contract => Model
 			this.CreateMap<GenreFormContract, Genre>();
-
 			// Genres: Model => Contract
 			this.CreateMap<Genre, GenreFormContract>();
 
 			// Genres: Contract => Contract
 			this.CreateMap<GenreFormContract, GenreDetailContract>();
-
 			// Genres: Contract => Contract
 			this.CreateMap<GenreDetailContract, GenreFormContract>();
+			#endregion
+
+			#region [Contracts: Movies]
+			// Movies: Model => Contract
+			this.CreateMap<Movie, MovieDetailContract>();
+			// Movies: Model => Contract
+			this.CreateMap<Movie, MovieListContract>();
+
+			// Movies: Contract => Model
+			this.CreateMap<MovieFormContract, Movie>();
+			// Movies: Model => Contract
+			this.CreateMap<Movie, MovieFormContract>();
+
+			// Movies: Contract => Contract
+			this.CreateMap<MovieFormContract, MovieDetailContract>();
+			// Movies: Contract => Contract
+			this.CreateMap<MovieDetailContract, MovieFormContract>();
+			#endregion
+
+			#region [Contracts: Persons]
+			// Persons: Model => Contract
+			this.CreateMap<Person, PersonDetailContract>();
+			// Persons: Model => Contract
+			this.CreateMap<Person, PersonListContract>();
+
+			// Persons: Contract => Model
+			this.CreateMap<PersonFormContract, Person>();
+			// Persons: Model => Contract
+			this.CreateMap<Person, PersonFormContract>();
+
+			// Persons: Contract => Contract
+			this.CreateMap<PersonFormContract, PersonDetailContract>();
+			// Persons: Contract => Contract
+			this.CreateMap<PersonDetailContract, PersonFormContract>();
 			#endregion
 		}
 		#endregion
