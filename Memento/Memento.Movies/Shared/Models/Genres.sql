ac077ea7f3163054857bca8355ec243889aef815
Tiago Reis <nuno-tiago-reis@outlook.pt>
2020-05-29 20:29:30 +0100

    Seeding:
    Added SQL seeding files.

---
+++ Memento/Memento.Movies/Shared/Models/Genres.sql
@@ -0,0 +1,14 @@
+INSERT INTO [dbo].[Genres]
+([CreatedBy], [CreatedAt], [UpdatedBy], [UpdatedAt], [Name], [NormalizedName])
+VALUES
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Action', 'ACTION'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Adventure', 'ADVENTURE'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Comedy', 'COMEDY'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Crime', 'CRIME'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Drama', 'DRAMA'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Historical', 'HISTORICAL'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Horror', 'HORROR')
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Musical', 'MUSICAL'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Science Fiction', 'SCIENCE FICTION'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'War', 'WAR'),
+(0, N'2020-05-31 00:00:00', NULL, NULL, 'Western', 'WESTERN')
